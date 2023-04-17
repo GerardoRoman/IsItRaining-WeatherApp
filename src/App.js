72167a23ce44f083b28a0db61be26dbeafdd8f5a
esantb <e.saint.bou@gmail.com>
2023-04-17 16:20:57 -0500

    daily weather map working, weekly map in progress

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import Forecast from './components/forecast.js'
-import React, { useEffect, useState } from 'react'
+import DailyWeather from './components/dailyWeather.js'
+import WeeklyWeather from './components/weeklyWeather.js'
 
 function App() {
-  const [lat, setLat] = useState([])
-  const [long, setLong] = useState([])
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      });
-
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
-      });
-    }
-    fetchData();
-  }, [lat,long])
 
   return (
-    <div className='home-component'>
-      <h1>Is It Raining... ?</h1>
-      <Forecast />
-    </div>
+    <DailyWeather />
+    // <WeeklyWeather />
   );
 }
 
