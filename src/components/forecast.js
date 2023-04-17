5bb1b7bf0d0354804d148ba0cebc2ec314cdc3d2
GerardoRoman <offthegrd3657@gmail.com>
2023-04-17 10:37:33 -0400

    added geolocation stuff

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 function Forecast() {
     const [weather, setWeather] = useState('')
     const [image, setImage] = useState('')
+  
 
     return (
+        <div>
+        </div>
     )
 }
+
+export default Forecast;
