d58cf7ecba2a53fcf85d39d2e83663663e1c8387
GerardoRoman <offthegrd3657@gmail.com>
2023-04-18 16:42:11 -0400

    did some css work to get the background image in the background and the t-rex in a natural position

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import DailyWeather from './components/dailyWeather.js'
 import WeeklyWeather from './components/weeklyWeather.js'
 import HourlyWeather from './components/dailyWeather.js'
 import myImage from './assets/trex_a_1_by_cleopatrawolf_dfujqzu-350t.png'
+import image from './assets/4_by_cleopatrawolf_dfuq86t.png'
+import css from './App.css'
 
 function App() {
 
@@ -11,9 +13,10 @@ function App() {
       <h1 className='app-title'>Is It Raining... ?</h1>
       {/* <DailyWeather /> */}
       {/* <WeeklyWeather /> */}
-      <img className='t-rex' src={myImage} alt='t=rex'></img>
       {/* <HourlyWeather /> */}
-      <WeeklyWeather />
+      {/* <WeeklyWeather /> */}
+      <img className='t-rex' src={myImage} alt='t=rex'></img>
+      <div style={{ backgroundImage: `url(${image})`,backgroundRepeat:"no-repeat",backgroundSize:"contain",height:1200,width:720}}></div>
 
     </div>
   );
