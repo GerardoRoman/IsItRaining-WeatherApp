9de8fc110720816160bb80bcdea994dd0f0ad285
GerardoRoman <offthegrd3657@gmail.com>
2023-04-19 19:05:34 -0400

    organized the pics into categories in the assets folder, adjusted the css on the weather forecast component to be ON the background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import CurrentWeather from './components/currentWeather.js'
 import WeeklyWeather from './components/weeklyWeather.js'
 import HourlyWeather from './components/hourlyWeather.js'
-import myImage from './assets/trex_a_1_by_cleopatrawolf_dfujqzu-350t.png'
-import image from './assets/4_by_cleopatrawolf_dfuq86t.png'
 import css from './App.css'
+import blueTrexRoar from './assets/trexImages/trex_a_1_by_cleopatrawolf_dfujqzu-350t.png'
+import mainBackgroundImage from './assets/backgroundImages/4_edit_by_cleopatrawolf_dfut2ry.png'
 
 function App() {
 
@@ -13,8 +13,9 @@ function App() {
       <CurrentWeather />
       {/* <WeeklyWeather /> */}
       {/* <HourlyWeather /> */}
-      <img className='t-rex' src={myImage} alt='t=rex'></img>
-      <div style={{ backgroundImage: `url(${image})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", height: 1200, width: 720 }}></div>
+      <img className='blue-trex-roar' src={blueTrexRoar} alt="Blue T-Rex Roaring"></img>
+      
+      <div style={{ backgroundImage: `url(${mainBackgroundImage})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", height: 1200, width: 720 }}></div>
 
     </div>
   );
--- src/components/animals.js
+++ src/components/animals.js
@@ -1,11 +1,46 @@
 
 function Animals() {
-    return(
-        <div>
-            <img className='t-rex' src={myImage} alt='t=rex'></img>
+    
+export const trexImages = {
+        blueTrexRoar: './assets/trex_a_1_by_cleopatrawolf_dfujqzu-350t.png',
+        blueTrexClosed: 'assets/trex_a_2_by_cleopatrawolf_dfujqzr-350t.png',
+        greenTrexRoar: 'assets/trex_b_1_by_cleopatrawolf_dfujqzp-350t.png',
+        greenTrexClosed: 'assets/trex_b_2_by_cleopatrawolf_dfujqzl-350t.png',
+        purpTrexRoar: 'assets/trex_c_1_by_cleopatrawolf_dfujqzg-350t.png',
+        purpTrexClosed: 'assets/trex_c_2_by_cleopatrawolf_dfujqzf-350t.png',
+        redTrexRoar: 'assets/trex_d_1_by_cleopatrawolf_dfujqzb-350t.png',
+        redTrexClosed: 'assets/trex_d_2_by_cleopatrawolf_dfujqz8-350t.png',
+    }
 
-        </div>
-    )
+export const toucanImages = {
+        blueToucanRoar: './assets/toucan_a_1_by_cleopatrawolf_dfujr0f-350t.png',
+        blueToucanClosed: './assets/toucan_a_2_by_cleopatrawolf_dfujr0c-350t.png',
+        purpToucanRoar: './assets/toucan_b_1_by_cleopatrawolf_dfujr08-350t.png',
+        purpToucanClosed: './assets/toucan_b_2_by_cleopatrawolf_dfujr04-350t.png',
+        yellowToucanRoar: './assets/toucan_c_1_by_cleopatrawolf_dfujr00-350t.png',
+        yellowToucanClosed: './assets/toucan_c_2_by_cleopatrawolf_dfujqzx-350t.png',
+    }
+
+export const stegoImages = {
+        greenStegoClosed: './assets/stego_a_1_by_cleopatrawolf_dfujr15-350t.png',
+        greenStegoRoar: './assets/stego_a_2_by_cleopatrawolf_dfujr13-350t.png',
+        purpStegoClosed: './assets/stego_b_1_by_cleopatrawolf_dfujr0z-350t.png',
+        purpStegoRoar: './assets/stego_b_2_by_cleopatrawolf_dfujr0v-350t.png',
+        pinkStegoClosed: './assets/stego_c_1_by_cleopatrawolf_dfujr0s-350t.png',
+        pinkStegoRoar: './assets/stego_c_2_by_cleopatrawolf_dfujr0p-350t.png',
+        brownStegoClosed: './assets/stego_d_1_by_cleopatrawolf_dfujr0m-350t.png',
+        brownStegoRoar: './assets/stego_d_2_by_cleopatrawolf_dfujr0i-350t.png',
+    }
+
+export const goatImages = {
+        blueGoatRoar: './assets/goat_a_1_by_cleopatrawolf_dfujr1n-350t.png',
+        blueGoatClosed: './assets/goat_a_2_by_cleopatrawolf_dfujr1j-350t.png',
+        greenGoatRoar: './assets/goat_b_1_by_cleopatrawolf_dfujr1h-350t.png',
+        greenGoatClosed: './assets/goat_b_2_by_cleopatrawolf_dfujr1e-350t.png',
+        pinkGoatRoar: './assets/goat_c_1_by_cleopatrawolf_dfujr1b-350t.png',
+        pinkGoatClosed: './assets/goat_c_2_by_cleopatrawolf_dfujr18-350t.png',
+    }
 }
 
-export default Animals 
+    // /Users/dorito_roman/Desktop/finalproject/friez/src/assets/stego_d_1_by_cleopatrawolf_dfujr0m-350t.png
+    // src/assets/stego_d_1_by_cleopatrawolf_dfujr0m-350t.png
