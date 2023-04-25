7031ba0eb8beb6994371a3e7fbd5d4ebdbaf348b
GerardoRoman <offthegrd3657@gmail.com>
2023-04-25 10:44:01 -0400

    fixed routing stuff, and created a home.js to move all of the app.js

--- src/components/animalLobby.js
+++ src/components/animalLobby.js
@@ -5,7 +5,7 @@ import { CgProfile } from 'react-icons/cg'
 import { IconContext } from 'react-icons'
 import "../styles/profile-icon.css"
 import "../styles/animalLobby.css"
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 // export const GoBack = () => {
 //     const navigate = useNavigate()
@@ -23,30 +23,30 @@ export default function AnimalLobby() { //add token, { username }
 
     // console.log(username.username)
     
-    useEffect(() => {
-        axios.get('https://is-it-raining.herokuapp.com/my-animals/', {
-            // headers: {
-            //     'Authorization': `Token ${token}`
-            // }
-        }).then((response) => {
-            setAnimalList(response.data)
-        })
+    // useEffect(() => {
+    //     axios.get('https://is-it-raining.herokuapp.com/my-animals/', {
+    //         // headers: {
+    //         //     'Authorization': `Token ${token}`
+    //         // }
+    //     }).then((response) => {
+    //         setAnimalList(response.data)
+    //     })
 
-        setAnimalId(animalList.id)
-    }, [])
+    //     setAnimalId(animalList.id)
+    // }, [])
 
     console.log(animalList)
     console.log(animalId)
 
-    function deleteAnimal(animalId) {
-        console.log(animalId)
-        axios.delete(`https://is-it-raining.herokuapp.com/captured/${animalId}`, {
-            // headers: {
-            //     'Authorization': `Token ${token}`
-            // }
-        })
-            .then(() => setAnimalList((animalList) => animalList.filter((animal) => animal.id !== animalId)))
-    }
+    // function deleteAnimal(animalId) {
+    //     console.log(animalId)
+    //     axios.delete(`https://is-it-raining.herokuapp.com/captured/${animalId}`, {
+    //         // headers: {
+    //         //     'Authorization': `Token ${token}`
+    //         // }
+    //     })
+    //         .then(() => setAnimalList((animalList) => animalList.filter((animal) => animal.id !== animalId)))
+    // }
 
     return (
         <div>
@@ -60,6 +60,9 @@ export default function AnimalLobby() { //add token, { username }
                 <div>
                     
                 </div>
+                <Link to='/weather'>
+                    <button>To Weather!</button>
+                </Link>
             </div>
             <div className='lobby-background-image'>
                 <img src={animalLobbyBackgroundImage} alt='profile-background'></img>
--- src/components/home.js
+++ src/components/home.js
@@ -8,14 +8,24 @@ import Registration from './components/registration.js'
 import { Routes, Route, Link } from 'react-router-dom'
 import AnimalLobby from './components/animalLobby.js'
 import mainBackgroundImage from './assets/backgroundImages/4_edit_by_cleopatrawolf_dfut2ry.png'
+// import { useNavigate } from 'react-router-dom'
 
+// export const ToAL = () => {
+//   const navigate = useNavigate()
+
+//   return(
+//       <>
+//       <button onClick={() => navigate('animal-lobby', { replace: true })}>To animal lobby!</button>
+//       </>
+//   )
+// }
 
 function App() {
-  const [lat, setLat] = useState(null)
-  const [long, setLong] = useState(null)
-  const [token, setToken] = useLocalStorageState('loginToken', '')
-  const [username, setUsername] = useLocalStorageState('userUsername', '')
-  const [hourlyTemps, setHourlyTemps] = useState([])
+    const [lat, setLat] = useState(null)
+    const [long, setLong] = useState(null)
+    const [token, setToken] = useLocalStorageState('loginToken', '')
+    const [username, setUsername] = useLocalStorageState('userUsername', '')
+    const [hourlyTemps, setHourlyTemps] = useState([])
 
 
   // const setAuth = (token, username) => {
@@ -24,35 +34,35 @@ function App() {
   // }
 
 
-  useEffect(() => {
+    useEffect(() => {
     const getData = async () => {
-      navigator.geolocation.getCurrentPosition(function (position) {
+        navigator.geolocation.getCurrentPosition(function (position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
-      });
+    });
       // console.log("Latitude is:", lat)
       // console.log("Longitude is:", long)
     }
     getData();
-  }, [lat, long]);
+}, [lat, long]);
 
 
-  return (
+    return (
     <>
-      <div>
+        <div>
         <Link to='/animal-lobby'>
         <button>Go to Animal Lobby!</button>
         </Link>
-      <Routes>
+        <Routes>
         <Route path='/animal-lobby' element={<AnimalLobby />} />
-      </Routes>
-      </div>
-      <CurrentWeather lat={lat} long={long} hourlyTemps={hourlyTemps} />
-      <HourlyWeather lat={lat} long={long} setHourlyTemps={setHourlyTemps} />
-      <div className="backgroundImage" style={{ backgroundImage: `url(${mainBackgroundImage})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", height: 1200, width: 720 }}></div>
-      {/* <Login setAuth={setAuth} />
+        </Routes>
+        </div>
+        <CurrentWeather lat={lat} long={long} hourlyTemps={hourlyTemps} />
+        <HourlyWeather lat={lat} long={long} setHourlyTemps={setHourlyTemps} />
+        <div className="backgroundImage" style={{ backgroundImage: `url(${mainBackgroundImage})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", height: 1200, width: 720 }}></div>
+        {/* <Login setAuth={setAuth} />
       <Registration setAuth={setAuth} /> */}
     </>
-  );
+    );
 }
 export default App;
